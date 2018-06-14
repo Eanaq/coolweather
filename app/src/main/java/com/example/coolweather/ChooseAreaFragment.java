e1ee901501ce7918e7bc015dde8fbd883d83c153
Eanaq <1127575703@qq.com>
2018-06-14 08:57:13 +0800

    完成遍历省市级三级列表的功能。

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
+import com.example.coolweather.db.Province;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
 
@@ -28,8 +29,6 @@ import java.util.List;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
-
-import static android.R.attr.type;
 
 /**
  * Created by A11 on 2018/6/13.
@@ -71,7 +70,7 @@ public class ChooseAreaFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    public void onActivityCreated( Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -91,17 +90,33 @@ public class ChooseAreaFragment extends Fragment {
                 if(currentLevel==LEVEL_COUNTY){
                     queryCities();
                 }else if(currentLevel==LEVEL_CITY){
-                    queryProvince();
+                    queryProvinces();
                 }
             }
         });
-        queryProvince();
+        queryProvinces();
     }
-
+    private void queryProvinces() {
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList=DataSupport.findAll(Province.class);
+        if (provinceList.size()>0){
+            dataList.clear();
+            for (Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_PROVINCE;
+        }else {
+            String address="https://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
     private void queryCities() {
         titleText.setText(selectedProvince.getProvinceName());
-        backButton.setVisibility(View.INVISIBLE);
-        cityList= DataSupport.where("province=?",String.valueOf(selectedProvince.getId())).find(City.class);
+        backButton.setVisibility(View.VISIBLE);
+        cityList= DataSupport.where("province = ?",String.valueOf(selectedProvince.getId())).find(City.class);
         if (cityList.size()>0){
             dataList.clear();
             for (City city:cityList){
@@ -116,8 +131,27 @@ public class ChooseAreaFragment extends Fragment {
             queryFromServer(address,"city");
         }
     }
-//根据传入地址和类型从服务器上查询省市级数据
-    private void queryFromServer(String address, String city) {
+    private void queryCounties() {
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList=DataSupport.where("city = ?",String.valueOf(selectedCity.getId())).find(County.class);
+        if (countyList.size()>0){
+            dataList.clear();
+            for (County county:countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel=LEVEL_COUNTY;
+        }else {
+            int provinceCode=selectedProvince.getProvinceCode();
+            int cityCode=selectedCity.getCityCode();
+            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
+            queryFromServer(address,"county");
+        }
+    }
+    //根据传入地址和类型从服务器上查询省市级数据
+    private void queryFromServer(String address, final String type) {
         showProgressDialog();
         HttpUtil.sendOkHttpRequest(address, new Callback() {
             @Override
@@ -148,7 +182,7 @@ public class ChooseAreaFragment extends Fragment {
                         public void run() {
                             closeProgressDialog();
                             if ("province".equals(type)){
-                                queryProvince();
+                                queryProvinces();
                             }else if ("city".equals(type)){
                                 queryCities();
                             }else if ("county".equals(type)){
@@ -162,43 +196,9 @@ public class ChooseAreaFragment extends Fragment {
 
     }
 
-    private void queryCounties() {
-        titleText.setText(selectedCity.getCityName());
-        backButton.setVisibility(View.INVISIBLE);
-        countyList=DataSupport.where("city=?",String.valueOf(selectedCity.getId())).find(County.class);
-        if (countyList.size()>0){
-            dataList.clear();
-            for (County county:countyList){
-                dataList.add(county.getCountyName());
-            }
-            adapter.notifyDataSetChanged();
-            listView.setSelection(0);
-            currentLevel=LEVEL_COUNTY;
-        }else {
-            int provinceCode=selectedProvince.getProvinceCode();
-            int cityCode=selectedCity.getCityCode();
-            String address="http://guolin.tech/api/china/"+provinceCode+"/"+cityCode;
-            queryFromServer(address,"county");
-        }
-    }
 
-    private void queryProvince() {
-        titleText.setText("中国");
-        backButton.setVisibility(View.GONE);
-        provinceList=DataSupport.findAll(Province.class);
-        if (provinceList.size()>0){
-            dataList.clear();
-            for (Province province:provinceList){
-                dataList.add(province.getProvinceName());
-            }
-            adapter.notifyDataSetChanged();
-            listView.setSelection(0);
-            currentLevel=LEVEL_PROVINCE;
-        }else {
-            String address="https://guolin.tech/api/china";
-            queryFromServer(address,"province");
-        }
-    }
+
+
 
     private void showProgressDialog() {
         if (progressDialog==null){
@@ -214,3 +214,4 @@ public class ChooseAreaFragment extends Fragment {
         }
     }
 }
+
